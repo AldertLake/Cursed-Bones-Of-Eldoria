56d4d7c1b50ff6474533f273d0858428007e7f8a
EliasWss :) <103462735+EliasWss@users.noreply.github.com>
2023-08-23 18:37:55 +0100

    Some Starter Stuffs

---
+++ Arcanelogic.cpp
@@ -0,0 +1,33 @@
+#include enginesource.cpp
+#include lightingft.calculute.cpp
+#include widgetunrealfeature.cpp
+
+using namespace::STD;
+
+
+int anfunctionthatgivesomthingLOL() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0;
+}
---
+++ DeffendLogic.cpp
@@ -0,0 +1,22 @@
+#include UnrealEngine.happen
+#include LightingLibrary.h
+#include <iostream>
+#include <enginesource>
+
+using namespace STD;
+
+float AttackEnemy()  {
+
+    HachRick() = false;
+    
+    return 0;
+}
+
+int getplayerattitude() {
+
+    set Altitude() = false;
+
+    Get Value ("NewAttitude")
+
+    return 0;
+ }
